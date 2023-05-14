aca00a3b28f452abb4a4e781fecce57f923873c4
zishuQ <1574908708@qq.com>
2023-05-14 13:33:43 +0800

    update some games

---
+++ llk/js/config.js
@@ -0,0 +1,33 @@
+
+var config = (function(){
+
+    time = 60;
+    var row = 8;
+    var col = 7;
+    var objectCount = 14;
+    var repeatCount = row * col / objectCount;
+    var imgUrl = "./img/";
+    var imgExtension = ".png";
+    var imgByName = function (name) {
+        var src = imgUrl + name + imgExtension;
+        return `<img draggable="false" src="${src}"></img>`;
+    }
+
+    var itemDirectionHTML = ` <div class="grid-item-direction">
+                                    <div class="y up"></div>
+                                    <div class="y down"></div>
+                                    <div class="x left"></div>
+                                    <div class="x right"></div>
+                              </div>`;
+
+    return {
+        row : row,
+        col : col,
+        objectCount : objectCount,
+        repeatCount: repeatCount, 
+        imgByName : imgByName,
+        itemDirectionHTML: itemDirectionHTML,
+        time : time,
+    }
+
+})();
---
+++ llk/js/main.js
@@ -0,0 +1,6 @@
+on(window,'load',function(){
+
+    var game = new Game();
+    game.setup();
+    event(game);
+});
